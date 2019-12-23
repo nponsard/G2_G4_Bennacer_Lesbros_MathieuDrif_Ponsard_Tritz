7e98121e209650a9f9615b25d4a7c190ee7a9a08
cmdetu <clement.mathieu_drif@outlook.fr>
2019-12-23 14:56:06 +0100

    positions, emplacement score, déplacement missiles

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-void initSpaceInvaders(spaceInvaders & SI)
+void initSpaceInvaders(spaceInvaders & SI, const unsigned & height, const unsigned & width)
 {
     figure player;
     player.Add(rectangle(pos(0, 0), 110, 30, KGreen, KGreen));
@@ -23,57 +23,113 @@ void initSpaceInvaders(spaceInvaders & SI)
 
 
     figure invader;
-    invader.Add(rectangle(pos(0, 460), 110, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(20, 450), 70, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(0, 450), 10, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(100, 450), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(0, 30), 110, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(20, 20), 70, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(0, 20), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(100, 20), 10, 10, KWhite, KWhite));
 
-    invader.Add(rectangle(pos(0, 440), 10, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(100, 440), 10, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(20, 440), 10, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(80, 440), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(0, 10), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(100, 10), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(20, 10), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(80, 10), 10, 10, KWhite, KWhite));
 
-    invader.Add(rectangle(pos(60, 430), 20, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(30, 430), 20, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(60, 0), 20, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(30, 0), 20, 10, KWhite, KWhite));
 
-    invader.Add(rectangle(pos(10, 470), 20, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(80, 470), 20, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(40, 470), 30, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(10, 40), 20, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(80, 40), 20, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(40, 40), 30, 10, KWhite, KWhite));
 
-    invader.Add(rectangle(pos(20, 480), 70, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(20, 50), 70, 10, KWhite, KWhite));
 
-    invader.Add(rectangle(pos(30, 490), 10, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(70, 490), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(30, 60), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(70, 60), 10, 10, KWhite, KWhite));
 
-    invader.Add(rectangle(pos(20, 500), 10, 10, KWhite, KWhite));
-    invader.Add(rectangle(pos(80, 500), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(20, 70), 10, 10, KWhite, KWhite));
+    invader.Add(rectangle(pos(80, 70), 10, 10, KWhite, KWhite));
 
 
 
 
     figure playerTorpedo;
-    playerTorpedo.Add(rectangle(pos(352, 180), 5, 30, KWhite, KWhite));
+    playerTorpedo.Add(rectangle(pos(0, 0), 5, 30, KWhite, KWhite));
 
 
     figure invadersTorpedo;
-    invadersTorpedo.Add(rectangle(pos(252, 180), 5, 30, KWhite, KWhite));
-    invadersTorpedo.Add(rectangle(pos(247, 210), 15, 5, KWhite, KWhite));
+    invadersTorpedo.Add(rectangle(pos(5, 0), 5, 30, KWhite, KWhite));
+    invadersTorpedo.Add(rectangle(pos(0, 30), 15, 5, KWhite, KWhite));
 
     SI.player = player;
     SI.invaders = invader;
     SI.invadersTorpedo = invadersTorpedo;
     SI.playerTorpedo = playerTorpedo;
+
+    SI.playerPos = pos(0, 50);
+
+    SI.invadersPos.push_back(pos(0, height-150));
+    SI.invadersTorpedoPos.push_back(pos(47, height-150));
 }
 
+void display(minGL & window, const vector<pos> & positions, const figure & fig)
+{
+    for(vector<pos>::const_iterator it(positions.begin()); it != positions.end(); ++it)
+        window << fig + *it;
+}
 
+void displaySpace(minGL & window, const spaceInvaders & SI)
+{
+    display(window, SI.invadersPos, SI.invaders);
+    display(window, SI.playerTorpedoPos, SI.playerTorpedo);
+    display(window, SI.invadersTorpedoPos, SI.invadersTorpedo);
+    window << SI.player + SI.playerPos;
+
+}
+
+void displayScore(minGL & window)
+{
+    window << rectangle(pos(0, window.getWindowHeight()), pos(window.getWindowWidth()-1, window.getWindowHeight() - 50), KGreen, KBlack);
+    window << rectangle(pos(0, 0), pos(window.getWindowWidth()-1, 50), KGreen, KBlack);
+}
+
+void process(spaceInvaders & SI, const unsigned & height, const unsigned & width)
+{
+    vector<pos>::iterator it(SI.playerTorpedoPos.begin());
+    while(it != SI.playerTorpedoPos.end())
+    {
+        if(it->getOrd() + 30 < height)
+        {
+            *it = *it + pos(0, 5);
+            ++it;
+        }
+        else
+            SI.playerTorpedoPos.erase(it);
+    }
+
+    it = SI.invadersTorpedoPos.begin();
+    while(it != SI.invadersTorpedoPos.end())
+    {
+        if(it->getOrd() > 0)
+        {
+            *it = pos(it->getAbs(), it->getOrd() - 1);
+            ++it;
+        }
+        else
+            SI.invadersTorpedoPos.erase(it);
+    }
+
+
+
+    /*GESTION POSITION JOUEUR*/
+}
 
 void mainSpaceInvaders()
 {
+    minGL window(1280, 720, "Space Invader", KBlack) ;
+    window.initGlut();
+    window.initGraphic();
+
     spaceInvaders SI;
-    initSpaceInvaders(SI);
-    minGL Window(800, 600, "Space Invader", KBlack) ;
-    Window.initGlut();
-    Window.initGraphic();
+    initSpaceInvaders(SI, window.getWindowHeight(), window.getWindowWidth());
 
     const chrono::duration<double, milli> frameDuration(16);//60fps
 
@@ -83,20 +139,22 @@ void mainSpaceInvaders()
         chrono::time_point<chrono::steady_clock> beg(chrono::steady_clock::now());
 
 
-        keyboardMap = Window.get_key_2();
+        keyboardMap = window.get_key_2();/*faire une methode dans MINGL*/
 
-        if(keyboardMap.find(100) != keyboardMap.end() && keyboardMap[100] && SI.playerPos.getAbs() + 120 < Window.getWindowWidth())
+        if(keyboardMap.find(100) != keyboardMap.end() && keyboardMap[100] && SI.playerPos.getAbs() + 120 < window.getWindowWidth())
             SI.playerPos.abs += 10;
         if(keyboardMap.find(113) != keyboardMap.end() && keyboardMap[113] && SI.playerPos.getAbs() > 10)
             SI.playerPos.abs -= 10;
-        if(keyboardMap.find(115) != keyboardMap.end())
-            SI.playerTorpedoPos.push_back(SI.playerPos + pos(0, 5));
+        if(keyboardMap.find(115) != keyboardMap.end() && keyboardMap[115])
+            SI.playerTorpedoPos.push_back(SI.playerPos + pos(52, 50));
 
-        Window.clearScreen();
-        Window << SI.invaders << SI.player + SI.playerPos << SI.playerTorpedo << SI.invadersTorpedo;
-        Window.updateGraphic();
+        window.clearScreen();
+        process(SI, window.getWindowHeight(), window.getWindowWidth());
+        displaySpace(window, SI);
+        displayScore(window);
 
-        //cout << SI.playerPos.abs << endl;
+        window.updateGraphic();
+
         chrono::time_point<chrono::steady_clock> end(chrono::steady_clock::now());
         chrono::duration<double, milli> diff(end - beg);
         if(diff < frameDuration)
@@ -104,12 +162,7 @@ void mainSpaceInvaders()
         end = chrono::steady_clock::now();
         //cout << chrono::duration<double, milli>(end - beg).count() << endl;
     }
-
-
-
-
-
-    Window.get_key();
+    window.get_key();
 }
 
 
