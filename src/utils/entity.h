b1d9742235a349604c26ea0796ea139532d51d92
Nils Ponsard <nils.ponsard@etu.univ-amu.fr>
2020-01-09 16:17:08 +0100

    modification de entity.h

--- src/utils/entity.h
+++ src/utils/entity.h
@@ -2,13 +2,26 @@
 #define ENTITY_H
 
 #include "minGL/figs/figure.h"
+#include <vector>
+#include "minGL/figs/pos.h"
 
 struct entity
 {
     unsigned entityHeight;
     unsigned entityWidth;
+    pos position;
 
-    figure entityFig;
+    unsigned entityFigID;
+};
+
+struct alien_entity
+{
+    unsigned entityHeight;
+    unsigned entityWidth;
+
+    unsigned entityFigID;
+    unsigned pathStep;
+    vector<pos> path;
 };
 
 #endif // ENTITY_H
